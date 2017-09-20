3be910f40a1def19f98d8443c23c11aeae6ffa7d
Arthur Gorr <art.gorr@gmail.com>
2017-09-20 07:51:31 -0500

    configure the application to be aware of SignalR and to forward SignalR related messages to the hub

--- ArthR.Next/Startup.cs
+++ ArthR.Next/Startup.cs
@@ -1,36 +1,29 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using ArthR.Next.Hubs;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
 
 namespace ArthR.Next
 {
     public class Startup
     {
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddSignalR();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
-            loggerFactory.AddConsole();
-
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
 
-            app.Run(async (context) =>
+            app.UseFileServer();
+
+            app.UseSignalR(routes =>
             {
-                await context.Response.WriteAsync("Hello World!");
+                routes.MapHub<ChatHub>("chat");
             });
         }
     }
